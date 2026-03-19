4f968e67ad0865dfc481cd139339e5b917087c3e
walle79 <sablade7@gmail.com>
2026-03-19 17:24:18 +0700

    update github action fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { 
   LayoutDashboard, Users, ReceiptText, 
   BarChart3, Plus, Search, UserPlus, UserCircle, Bell
